871ae6dbae0f3a88370c56de0bc9d411d4dfa282
Derek Gordon <dgordon@gss.local>
2024-02-21 10:10:23 -0800

    simplify to do while

--- src/Program.cs
+++ src/Program.cs
@@ -7,35 +7,22 @@ namespace program
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the amount financed: ");
-            double amountFinanced = double.Parse(Console.ReadLine());
-
-            Console.Write("Enter the rate of interest %: ");
-            double roi = double.Parse(Console.ReadLine());
-
-            Console.Write("Enter the term in years: ");
-            int termMonths = int.Parse(Console.ReadLine());
-
-            var loan = new Loan(amountFinanced, roi, termMonths);
-            Console.Write(loan.GetAmortizationSchedule());
-
-            Console.Write("Do Another Calculation? (Y/N): ");
-            while(Console.ReadLine().ToUpper() == "Y")
+            do
             {
                 Console.Write("Enter the amount financed: ");
-                amountFinanced = double.Parse(Console.ReadLine());
+                double amountFinanced = double.Parse(Console.ReadLine());
 
                 Console.Write("Enter the rate of interest %: ");
-                roi = double.Parse(Console.ReadLine());
+                double roi = double.Parse(Console.ReadLine());
 
                 Console.Write("Enter the term in years: ");
-                termMonths = int.Parse(Console.ReadLine());
+                int term = int.Parse(Console.ReadLine());
 
-                loan = new Loan(amountFinanced, roi, termMonths);
+                var loan = new Loan(amountFinanced, roi, term);
                 Console.Write(loan.GetAmortizationSchedule());
 
                 Console.Write("Do Another Calculation? (Y/N): ");
-            }
+            } while(Console.ReadLine().Equals("Y", StringComparison.CurrentCultureIgnoreCase));
         }
     }
 }
